00bb2013042526e39c36176ee551042814f6af19
Maciej Baba <maciek2baba2@gmail.com>
2024-11-11 22:31:13 +0100

    add some tests, change title

--- my-app/e2e/demo.test.ts
+++ my-app/e2e/demo.test.ts
@@ -4,3 +4,23 @@ test('home page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.locator('h1')).toBeVisible();
 });
+test('home page has expected title', async ({ page }) => {
+	await page.goto('/');
+	await expect(page).toHaveTitle('My App');
+})
+test('home page has expected text', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.locator('p')).toContainText('Welcome to SvelteKit');
+})
+test('about page has expected h1', async ({ page }) => {
+	await page.goto('/about');
+	await expect(page.locator('h1')).toBeVisible();
+});
+test('about page has expected title', async ({ page }) => {
+	await page.goto('/about');
+	await expect(page).toHaveTitle('About');
+})
+test('about page has expected text', async ({ page }) => {
+	await page.goto('/about');
+	await expect(page.locator('p')).toContainText('This is the about page');
+})
